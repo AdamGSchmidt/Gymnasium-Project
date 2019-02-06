38e2de9fdfc6e2fdce8f8daf6150bdd58d4efdb3
AdamGSchmidt <adam.schmidt.nacka@gmail.com>
2019-02-06 13:43:31 +0100

    Started menu canvas redesign 	modified:   client/css/menuStyle.css 	modified:   client/html/menu.html 	modified:   client/js/menuScript.js

--- client/js/menuScript.js
+++ client/js/menuScript.js
@@ -1,14 +1,55 @@
+// Globala variabler
+let canvasWidth;
+let canvasHeight;
+let c;
+let username;
+
+const defaoultScale = 680;
 
 
 const startup = () => {
-    // Hämtar användarnamn
+    // Hämntar användarnamn och anropar andera functioner
     getUsername();
 
     // hämtar antalet nuvarande spelare varje sekund
-    getNumberOfUsers();
+   /* getNumberOfUsers();
     setInterval(() => {
         getNumberOfUsers();
-    }, 1000);
+    }, 1000); */
+}
+
+const drawMenue = () => {
+    console.log(canvasHeight + " window size " + canvasWidth)
+    let ctx = c.getContext("2d");
+    ctx.clearRect(-10000, -10000, 26000, 26000);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // draw button boxes
+
+    ctx.fillStyle = "#D3D3D3";
+    ctx.fillRect(0, 0, canvasWidth / 4, canvasHeight / 10);
+
+    ctx.fillStyle = "#0000FF";
+    ctx.fillRect(0, canvasHeight / 10, canvasWidth / 8, (canvasHeight / 5));
+
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(0, (canvasHeight / 10) * 3, canvasWidth / 8, (canvasHeight / 5));
+
+    ctx.fillStyle = "#FFA500";
+    ctx.fillRect(0, (canvasHeight / 2), canvasWidth / 8, (canvasHeight / 5));
+
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, (canvasHeight / 10) * 7, canvasWidth / 8, (canvasHeight / 5));
+
+    ctx.fillStyle = "#FFFF00";
+    ctx.fillRect(0, (canvasHeight / 10) * 9, canvasWidth / 8, (canvasHeight / 10));
+
+    // username
+
+    ctx.font = `${15 * canvasWidth / defaoultScale}px Arial`;
+    ctx.textAlign = "center";
+    ctx.fillText(username, canvasWidth / 8, canvasHeight / 18);
 }
 
 const logout = () => {
@@ -33,11 +74,15 @@ const getUsername = () => {
         data: {},
         success: function (data) {
 
-            let username = data.Username;
-            setUser(username);
+            username = data.Username;
+            console.log(username)
+            resize();
+            drawMenue();
         },
         error: function (jqXHR, textStatus, err) {
             alert('Error');
+            resize();
+            drawMenue();
         }
     });
 }
@@ -62,21 +107,21 @@ const play = () => {
     window.location.replace(window.location.protocol + "//" + window.location.host + '/game');
 }
 
-const setUser = (username) => {
-    document.getElementById('usernameText').innerHTML = username;
-}
 
 const setNumberOfUsers = (numberOfUsers) => {
-    document.getElementById('numberOfUsersCount').innerHTML = numberOfUsers;
+    // document.getElementById('numberOfUsersCount').innerHTML = numberOfUsers;
 }
 
 const resize = () => {
-    let containerSize = document.getElementById('menuCanvasContainer');
-    let c = document.getElementById("menuCanvas");
-    let canvasWidth = containerSize.innerWidth;
-    let canvasHeight = containerSize.innerHeight;
-    c.style.width = canvasWidth;
-    c.style.height = canvasHeight;
+    c = document.getElementById("menuCanvas");
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    c.style.display = 'block';
+    c.width = canvasWidth;
+    c.height = canvasHeight;
+    drawMenue();
 }
 
-startup();
+window.onload = () => {
+    startup();
+}
